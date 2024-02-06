bdc6b023407515900da385bb04e91db32c5c1694
jcarlson <jake.carlson90@gmail.com>
2024-02-05 17:48:17 -0800

    frontend complete
    clear

---
+++ backend/main.py
@@ -0,0 +1,36 @@
+# uvicorn main:app
+# uvicorn main:app --reload
+
+from fastapi import FastAPI, File, UploadFile, HTTPException
+from fastapi.responses import StreamingResponse
+from fastapi.middleware.cors import CORSMiddleware
+from decouple import config
+import openai
+
+#custom fucntion imports
+
+#initiate App
+app = FastAPI()
+
+# CORS -Origins
+origins = [
+    "http://localhost:5173",
+    "http://localhost:5174",
+    "http://localhost:4173",
+    "http://localhost:4174",
+    "http://localhost:3000",
+]
+
+# CORS middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.get("/health")
+async def check_health():
+    return {"message": "Healthy"} 
---
+++ frontend/src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="App">
+      <div className="text-2xl bg-blue-500">Hello</div>
+    </div>
+  );
+}
+
+export default App;
